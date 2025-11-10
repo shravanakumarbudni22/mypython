3c163f1ce59e4626b2a6085aeaf521fcc3b4d0c8
shravanakumarbudni22 <shravanakumarbudni@gmail.com>
2025-11-10 16:31:23 +0530

    Create arthematic.py

---
+++ arthematic.py
@@ -0,0 +1,21 @@
+a=int(input("enter your age"))
+name=input("enter your name")
+weight=float(input("enter your weight"))
+print(a)
+print(name)
+print(weight)
+#addition
+c=a+weight
+print("adition is",c)
+#sub
+d=a-weight
+print("sub is",d)
+#div
+e=a/weight
+print("div is",e)
+#multi
+s=a*weight
+print("multi is",s)
+#floor div
+f=a//weight
+print("floor div is",f)
