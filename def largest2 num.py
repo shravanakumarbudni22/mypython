b0276339886e9d1020ce0c6ad847524d676c972c
shravanakumarbudni22 <shravanakumarbudni@gmail.com>
2025-11-12 16:24:14 +0530

    Create def largest2 num.py

---
+++ def largest2 num.py
@@ -0,0 +1,8 @@
+#Largest of two numbers
+def hi():
+  num = int(input("enter your number"))
+  if num >= 5:
+    print("it is largest number")
+  else:
+    print("it is not largest number")
+hi()      
